56eb223c8cd4f23c7d17a4aa0295cd768068f29e
codedefective <accounts@erdemakbulut.com.tr>
2022-03-16 09:49:26 +0300

    delete external bat file

--- run.bat
+++
@@ -1,3 +0,0 @@
-docker-compose exec --user devilbox php  /bin/sh -c "mysql -u root -h 127.0.0.1 -proot -e 'CREATE DATABASE IF NOT EXISTS '${MYSQL_DATABASE}';'"
-docker-compose exec --user devilbox php  /bin/sh -c "composer install --working-dir=/shared/httpd/test/htdocs;"
-docker-compose exec --user devilbox php  /bin/sh -c "php /shared/httpd/test/htdocs/artisan migrate:fresh --seed"
