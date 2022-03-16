d1105f56efca306835941400323c5ac30dcd4c37
codedefective <accounts@erdemakbulut.com.tr>
2022-03-16 10:33:36 +0300

    auto run sh update

--- cfg/php-startup-7.4/test.sh
+++ cfg/php-startup-7.4/test.sh
@@ -3,7 +3,5 @@
 #
 cp /shared/httpd/test/htdocs/.env.example /shared/httpd/test/htdocs/.env
 composer install --working-dir=/shared/httpd/test/htdocs
-wait 5
 mysql -u root -h 112.13.138.12 -proot -e 'CREATE DATABASE IF NOT EXISTS products'
-wait 5
 php /shared/httpd/test/htdocs/artisan migrate:fresh --seed
