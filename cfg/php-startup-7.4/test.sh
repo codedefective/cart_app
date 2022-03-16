25e4af6b1421e20d32dcfd21130e8ff630b7d95a
codedefective <accounts@erdemakbulut.com.tr>
2022-03-16 10:11:57 +0300

    auto run sh update

--- cfg/php-startup-7.4/test.sh
+++ cfg/php-startup-7.4/test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 mysql -u root -h 112.13.138.12 -proot -e 'CREATE DATABASE IF NOT EXISTS '${MYSQL_DATABASE}''
 cp /shared/httpd/test/htdocs/.env.example /shared/httpd/test/htdocs/.env
 composer install --working-dir=/shared/httpd/test/htdocs
